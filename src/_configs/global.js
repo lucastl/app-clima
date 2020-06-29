5b7bf0ea3da8014360154482f99a97d51664d62b
Lucas Tosello Latini <lucastosellolatini@gmail.com>
2020-06-28 21:20:11 -0300

    https añadido a url api

--- src/_configs/global.js
+++ src/_configs/global.js
@@ -1,4 +1,4 @@
 export const GLOBAL = {
-    'API_URL': 'http://api.openweathermap.org/data/2.5/group?id=',
+    'API_URL': 'https://api.openweathermap.org/data/2.5/group?id=',
     'API_KEY': '2ad18122de5c17510667bdf02bbc6b34',
 }
